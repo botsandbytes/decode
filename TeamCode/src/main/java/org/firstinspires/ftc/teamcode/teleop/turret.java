103f9d5db055551adcd3449be0986c05b5f874c8
pokeshah <89949620+pokeshah@users.noreply.github.com>
2025-11-16 16:34:36 -0800

    turret

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/turret.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.teleop;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class turret extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        final DcMotorEx leftFront, leftBack, rightBack, rightFront;
+        leftFront = hardwareMap.get(DcMotorEx.class, "shooter1");
+        leftBack = hardwareMap.get(DcMotorEx.class, "shooter2");
+        DcMotorEx feeder = hardwareMap.get(DcMotorEx.class, "shooter3");
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // TODO: reverse motor directions if needed
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            leftFront.setPower(1);
+            leftBack.setPower(1);
+            feeder.setPower(1);
+            telemetry.addData("fl", leftFront.getVelocity());
+            telemetry.addData("bl", leftBack.getVelocity());
+            telemetry.update();
+        }
+    }
+}
