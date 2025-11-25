4f89db93bd212fd39c219d61534ee8c15fcdd2a3
pokeshah <89949620+pokeshah@users.noreply.github.com>
2025-11-25 00:45:05 -0800

    fix maxvel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MaxVelocityTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MaxVelocityTest.java
@@ -10,15 +10,10 @@ public class MaxVelocityTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         final DcMotorEx leftFront, leftBack, rightBack, rightFront;
-        leftFront = hardwareMap.get(DcMotorEx.class, "shooter1");
-        leftBack = hardwareMap.get(DcMotorEx.class, "shooter2");
+        leftFront = hardwareMap.get(DcMotorEx.class, "shooter");
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        // TODO: reverse motor directions if needed
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
@@ -26,9 +21,7 @@ public class MaxVelocityTest extends LinearOpMode {
 
         while (opModeIsActive()) {
             leftFront.setPower(1);
-            leftBack.setPower(1);
             telemetry.addData("fl", leftFront.getVelocity());
-            telemetry.addData("bl", leftBack.getVelocity());
             telemetry.update();
         }
     }
