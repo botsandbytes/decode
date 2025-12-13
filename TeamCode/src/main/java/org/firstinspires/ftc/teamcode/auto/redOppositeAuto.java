ef1125de57d3fabbb291a08cdcbc3c56f3c263da
Swayam Rathi <141093024+SwayamRathi123@users.noreply.github.com>
2025-12-13 10:30:39 -0800

    Added code to decide row 3 based on flag and added option to cycle balls from opened gate

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/redOppositeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/redOppositeAuto.java
@@ -16,6 +16,7 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 @Autonomous(name = "Red Opposite Auto", group = "BB Auto")
 public class redOppositeAuto extends OpMode {
 
+    boolean pickupLine3 = true;
     double shootPower = 0.91;
     int waitTimeForLaunch = 5000;
     double transferPower = .12;
@@ -42,9 +43,11 @@ public class redOppositeAuto extends OpMode {
 //    private final Pose scorePose2 = new Pose(96, 96, Math.toRadians(45));// Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
     private final Pose pickup3Pose = new Pose(94, 60, Math.toRadians(0)); // Middle (Second Set) of Artifacts from the Spike Mark.
     private final Pose pickup3PoseEnd = new Pose(134, 58, Math.toRadians(0));
+    private final Pose pickup4Pose = new Pose(132, 26, Math.toRadians(0));
+    private final Pose pickup5Pose = new Pose(132, 44, Math.toRadians(0));
     //94,36
     private Path scorePreload;
-    private PathChain grabPickup1, grabPickup1Start, scorePickup1, grabPickup2, scorePickup2, grabPickup3, scorePickup3;
+    private PathChain grabPickup1, grabPickup1Start, scorePickup1, grabPickup2, scorePickup2, grabPickup3, scorePickup3, grabPickup4, scorePickup4, grabPickup5, scorePickup5;
 
     public void buildPaths() {
         /* This is our scorePreload path. We are using a BezierLine, which is a straight line. */
@@ -108,6 +111,28 @@ public class redOppositeAuto extends OpMode {
         scorePickup3 = follower.pathBuilder()
                 .addPath(new BezierLine(pickup3PoseEnd, scorePose))
                 .setLinearHeadingInterpolation(pickup3PoseEnd.getHeading(), scorePose.getHeading())
+                .build();
+
+        grabPickup4 = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, pickup4Pose))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup4Pose.getHeading())
+                .build();
+
+        /* This is our scorePickup3 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+        scorePickup4 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup4Pose, scorePose))
+                .setLinearHeadingInterpolation(pickup4Pose.getHeading(), scorePose.getHeading())
+                .build();
+
+        grabPickup5 = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, pickup5Pose))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup5Pose.getHeading())
+                .build();
+
+        /* This is our scorePickup3 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+        scorePickup5 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup5Pose, scorePose))
+                .setLinearHeadingInterpolation(pickup5Pose.getHeading(), scorePose.getHeading())
                 .build();
     }
 
@@ -179,35 +204,70 @@ public class redOppositeAuto extends OpMode {
                     intakeL.stopIntake();
                     intakeL.takeShot(shootPower, waitTimeForLaunch);
                     intakeL.stopLauncher();
-                    intakeL.runIntake(1, transferPower);
-                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
-                    follower.followPath(grabPickup3, true);
+                    if (pickupLine3) {
+                        intakeL.runIntake(1, transferPower);
+                        /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
+                        follower.followPath(grabPickup3, true);
+                    } else {
+                        intakeL.runIntake(1, transferPower);
+                        /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
+                        follower.followPath(grabPickup4, true);
+                    }
                     setPathState(6);
                 }
                 break;
             case 6:
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup3Pose's position */
                 if (!follower.isBusy()) {
-                    /* Grab Sample */
-                    intakeL.runIntake(1, transferPower);
+                    if (pickupLine3) {
+                        /* Grab Sample */
+                        intakeL.runIntake(1, transferPower);
 //                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
-                    follower.followPath(scorePickup3, true);
-                    intakeL.powerOnLauncher(shootPower);
+                        follower.followPath(scorePickup3, true);
+                        intakeL.powerOnLauncher(shootPower);
+                    } else {
+                        intakeL.runIntake(1, transferPower);
+                        follower.followPath(scorePickup4, true);
+                        intakeL.powerOnLauncher(shootPower);
+                    }
                     setPathState(7);
                 }
                 break;
             case 7:
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if (!follower.isBusy()) {
+                        /* Score Sample */
+                    intakeL.stopIntake();
+                    intakeL.takeShot(shootPower, waitTimeForLaunch);
+                    intakeL.stopLauncher();
+                    intakeL.runIntake(1, transferPower);
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
+                    follower.followPath(grabPickup5, .6,true);
+                    setPathState(8);
+                }
+                break;
+            case 8:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup2Pose's position */
+                if (!follower.isBusy()) {
+                    /* Grab Sample */
+                    intakeL.runIntake(1, transferPower);
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
+                    follower.followPath(scorePickup5, true);
+                    intakeL.powerOnLauncher(shootPower);
+                    setPathState(9);
+                }
+                break;
+            case 9:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
+                if (!follower.isBusy()) {
                     /* Score Sample */
                     intakeL.stopIntake();
                     intakeL.takeShot(shootPower, waitTimeForLaunch);
                     intakeL.stopLauncher();
-                    setPathState(8);
+                    setPathState(10);
                 }
                 break;
-
-            case 8:
+            case 10:
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
                 if (!follower.isBusy()) {
                     /* Set the state to a Case we won't use or define, so it just stops running an new paths */
