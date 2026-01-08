df4d6a2e47319b1d7d63e12524c7485fe44c38c6
Swayam Rathi <141093024+SwayamRathi123@users.noreply.github.com>
2026-01-08 09:20:25 -0800

    minor clean up for manual turret turn tester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/manualTurretTurnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/manualTurretTurnTest.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.tests;
 
-import static java.lang.Math.abs;
-
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.control.PIDFCoefficients;
@@ -10,15 +8,13 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
-
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @Configurable
-@TeleOp(name = "Manual Turn Tester", group = "Test")
+@TeleOp(name = "Manual Turret Turn Tester", group = "Test")
 // Comment this out to add to the OpMode list
 public class manualTurretTurnTest extends LinearOpMode
 {
@@ -28,7 +24,7 @@ public class manualTurretTurnTest extends LinearOpMode
     private TelemetryManager telemetryM;
     public static int turn_degrees = 60;
     public static double sleep_multiplier = 15.7;
-    PIDFCoefficients pidfCoefficients = new PIDFCoefficients(0.009, 0.0003, 0.003, 0.1); // (0.008, 0.0005, 0.003, 0.007);
+    public static PIDFCoefficients pidfCoefficients = new PIDFCoefficients(0.009, 0.0003, 0.003, 0.1); // (0.008, 0.0005, 0.003, 0.007);
     PIDFController pidfController = new PIDFController(pidfCoefficients);
 
     //----------------------------------------------------------------------------------------------
@@ -37,37 +33,23 @@ public class manualTurretTurnTest extends LinearOpMode
 
     public void turnUsingPIDF() {
         int error_tolerance = 2;
+        double maxPower = 0.5, minPower = 0.15;
         double currentAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
         pidfController.updatePosition(currentAngle);
         pidfController.setTargetPosition(turn_degrees);
-//        if (turn_degrees < 0) {
-//            turn.setDirection(CRServo.Direction.FORWARD);
-//            while (pidfController.getError() > 2 || pidfController.getError() < -2) {
-//                currentAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
-//                pidfController.updatePosition(currentAngle);
-//                double power = pidfController.run();
-//                telemetry.addData("Current Power", power);
-//                telemetry.addData("Error: ", pidfController.getError());
-//                telemetry.addData("Current Angle: ", currentAngle);
-//                telemetry.update();
-//                turn.setPower(power);
-//            }
-//            turn.setPower(0);
-//            pidfController.reset();
-//        } else {
             turn.setDirection(CRServo.Direction.FORWARD);
-            while (pidfController.getError() > 2 || pidfController.getError() < -2) {
+            while (pidfController.getError() > error_tolerance || pidfController.getError() < -error_tolerance) {
                 currentAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
                 pidfController.updatePosition(currentAngle);
                 double power = pidfController.run();
-                if (power > 0.5) {
-                    power = 0.5;
-                } else if (power < 0.15 && power > 0) {
-                    power = 0.15;
-                } else if (power > -0.15 && power < 0) {
-                    power = -0.15;
-                } else if (power < -0.5) {
-                    power = -0.5;
+                if (power > maxPower) {
+                    power = maxPower;
+                } else if (power < minPower && power > 0) {
+                    power = minPower;
+                } else if (power > -minPower && power < 0) {
+                    power = -minPower;
+                } else if (power < -maxPower) {
+                    power = -maxPower;
                 }
                 telemetry.addData("Current Power", power);
                 telemetry.addData("Error: ", pidfController.getError());
@@ -77,36 +59,6 @@ public class manualTurretTurnTest extends LinearOpMode
             }
             turn.setPower(0);
             pidfController.reset();
-//        }
-
-    }
-    public void turnTurret(int degrees) {
-        if (degrees > 0 ) {
-            turn.setDirection(DcMotorSimple.Direction.REVERSE);
-            turn.setPower(0.5);
-            while(true) {
-                YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-                double delta = abs(degrees - orientation.getYaw(AngleUnit.DEGREES));
-                if (delta < 2) {
-                    break;
-                }
-            }
-            turn.setPower(0);
-//            sleep((long) (degrees * sleep_multiplier));
-        } else {
-            turn.setDirection(DcMotorSimple.Direction.FORWARD);
-            turn.setPower(0.5);
-            while(true) {
-                YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-                double delta = abs(degrees - orientation.getYaw(AngleUnit.DEGREES));
-                if (delta < 2) {
-                    break;
-                }
-            }
-            turn.setPower(0);
-//            sleep((long) (-degrees * sleep_multiplier));
-        }
-        turn.setPower(0);
     }
 
     @Override public void runOpMode() throws InterruptedException {
@@ -116,23 +68,6 @@ public class manualTurretTurnTest extends LinearOpMode
         imu = hardwareMap.get(IMU.class, "turnImu");
         turn = hardwareMap.get(CRServo.class, "turn");
 
-
-        /* Define how the hub is mounted on the robot to get the correct Yaw, Pitch and Roll values.
-         *
-         * Two input parameters are required to fully specify the Orientation.
-         * The first parameter specifies the direction the printed logo on the Hub is pointing.
-         * The second parameter specifies the direction the USB connector on the Hub is pointing.
-         * All directions are relative to the robot, and left/right is as-viewed from behind the robot.
-         *
-         * If you are using a REV 9-Axis IMU, you can use the Rev9AxisImuOrientationOnRobot class instead of the
-         * RevHubOrientationOnRobot class, which has an I2cPortFacingDirection instead of a UsbFacingDirection.
-         */
-
-        /* The next two lines define Hub orientation.
-         * The Default Orientation (shown) is when a hub is mounted horizontally with the printed logo pointing UP and the USB port pointing FORWARD.
-         *
-         * To Do:  EDIT these two lines to match YOUR mounting configuration.
-         */
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
         RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
@@ -145,9 +80,6 @@ public class manualTurretTurnTest extends LinearOpMode
 
         // Loop and update the dashboard
         while (!isStopRequested()) {
-
-            telemetry.addData("Hub orientation", "Logo=%s   USB=%s\n ", logoDirection, usbDirection);
-
             // Check to see if heading reset is requested
             if (gamepad1.dpad_down) {
                 telemetry.addData("Yaw", "Resetting\n");
@@ -155,7 +87,6 @@ public class manualTurretTurnTest extends LinearOpMode
             } else  if (gamepad1.dpad_right) {
                 pidfController.setTargetPosition(turn_degrees);
                 turnUsingPIDF();
-                //turnTurret(turn_degrees);
             } else {
                 telemetry.addData("Yaw", "Press DPAD DOWN on Gamepad to reset\n");
             }
