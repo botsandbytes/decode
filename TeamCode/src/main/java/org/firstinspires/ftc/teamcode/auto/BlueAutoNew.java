ff8687314e3669cac0f83d8ecf902bdf764b06dc
Swayam Rathi <141093024+SwayamRathi123@users.noreply.github.com>
2026-01-15 09:01:29 -0800

    Create BlueAutoNew.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAutoNew.java
@@ -0,0 +1,345 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import static org.firstinspires.ftc.teamcode.teleop.BlueTeleOp.GOAL_X;
+import static org.firstinspires.ftc.teamcode.teleop.BlueTeleOp.GOAL_Y;
+import static java.lang.Thread.sleep;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.field.FieldManager;
+import com.bylazar.field.PanelsField;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.robot.IntakeLauncher;
+import org.firstinspires.ftc.teamcode.utilities.DrawingUtil;
+
+@Configurable
+@Autonomous(name = "Blue Auto NEW", group = "Blue Auto")
+public class BlueAutoNew extends OpMode {
+    public static double slowVelocity = 5;
+    public static long drinkWaitTime = 1250;
+    public static double shootWaitTime = 1250;
+    private IntakeLauncher intakeLauncher;
+    private Follower follower;
+    private Timer pathTimer;
+    private Timer opmodeTimer;
+    private int pathState;
+    public static final double launchPower = 0.7;
+    private final double transferPower = 0.12;
+
+    private final Pose startPose = new Pose(117, 128, Math.toRadians(45)).mirror();
+    private final Pose scorePose = new Pose(88, 80, Math.toRadians(52)).mirror();
+    private final Pose drinkPoseCP = new Pose(96, 72, Math.toRadians(42)).mirror();
+    private final Pose drinkPoseEnd = new Pose(129, 60.5, Math.toRadians(42)).mirror();
+    private final Pose pickup1PoseCP = new Pose(100, 84, Math.toRadians(0)).mirror();
+    private final Pose pickup1PoseEnd = new Pose(123, 84, Math.toRadians(0)).mirror();
+    private final Pose gateAfterPose1 = new Pose(103, 76, Math.toRadians(0)).mirror();
+    private final Pose pickup2PoseCP = new Pose(84, 55, Math.toRadians(0)).mirror();
+    private final Pose pickup2PoseEnd = new Pose(130, 60, Math.toRadians(0)).mirror();
+    private final Pose pickup3PoseCP = new Pose(88, 25, Math.toRadians(0)).mirror();
+    private final Pose pickup3PoseEnd = new Pose(130, 36, Math.toRadians(0)).mirror();
+
+    private Path scorePreload;
+    private PathChain grabPickup1, scorePickup1, drinkPickupStart, drinkPickupScore, grabPickup2, scorePickup2, grabPickup3, scorePickup3, gatePark;
+    private FieldManager field;
+
+    public void buildPaths() {
+        scorePreload = new Path(new BezierLine(startPose, scorePose));
+        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
+
+        drinkPickupStart = follower.pathBuilder()
+                .addPath(new BezierCurve(scorePose, drinkPoseCP, drinkPoseEnd ))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), drinkPoseEnd.getHeading())
+                .build();
+
+        drinkPickupScore = follower.pathBuilder()
+                .addPath(new BezierCurve(drinkPoseEnd, drinkPoseCP, scorePose ))
+                .setLinearHeadingInterpolation(drinkPoseEnd.getHeading(), scorePose.getHeading())
+                .build();
+
+        grabPickup1 = follower.pathBuilder()
+                .addPath(new BezierCurve(scorePose, pickup1PoseCP, pickup1PoseEnd ))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1PoseEnd.getHeading(), .1)
+                .build();
+
+        gatePark = follower.pathBuilder()
+                .addPath(new BezierCurve(scorePose, pickup1PoseCP, gateAfterPose1 ))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), gateAfterPose1.getHeading(), .1)
+                .build();
+
+        scorePickup1 = follower.pathBuilder()
+                .addPath(new BezierLine(pickup1PoseEnd, scorePose))
+                .setLinearHeadingInterpolation(pickup1PoseEnd.getHeading(), scorePose.getHeading())
+                .build();
+
+        grabPickup2 = follower.pathBuilder()
+                .addPath(new BezierCurve(scorePose, pickup2PoseCP, pickup2PoseEnd ))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup2PoseEnd.getHeading(), 0.5)
+                .build();
+
+
+        scorePickup2 = follower.pathBuilder()
+                .addPath(new BezierCurve(pickup2PoseEnd, pickup2PoseCP, scorePose ))
+                .setLinearHeadingInterpolation(pickup2PoseEnd.getHeading(), scorePose.getHeading())
+                .build();
+
+        grabPickup3 = follower.pathBuilder()
+                .addPath(new BezierCurve(scorePose, pickup3PoseCP, pickup3PoseEnd ))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup3PoseEnd.getHeading(), 0.5)
+                .build();
+
+        scorePickup3 = follower.pathBuilder()
+                .addPath(new BezierCurve(pickup3PoseEnd, scorePose))
+                .setLinearHeadingInterpolation(pickup3PoseEnd.getHeading(), scorePose.getHeading())
+                .build();
+    }
+
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0 -> {
+                // go to score preload location
+                intakeLauncher.powerOnLauncher(launchPower-0.02);
+                follower.followPath(scorePreload);
+                setPathState(1);
+            }
+            case 1 -> {
+                // score preload & go to pick up line 2
+                if (follower.atPose(scorePose, 1, 1)) {
+                    intakeLauncher.stopIntake();
+                    // score preload
+                    if (!intakeLauncher.isShooting()) {
+                        intakeLauncher.startShooting();
+                    }
+                    intakeLauncher.takeShot(launchPower);
+//                    intakeLauncher.updateShootingLogic(launchPower, follower.getPose());
+
+                    // stop shooting and go for drink pick up 1
+                    if (intakeLauncher.getShootingDuration() > shootWaitTime) {
+                        intakeLauncher.stopShooting();
+                        intakeLauncher.runIntake(1, transferPower);
+                        follower.followPath(grabPickup2, true);
+                        setPathState(2);
+                    }
+                }
+            }
+            case 2 -> {
+                // go to score pose for line 2
+                if (!follower.isBusy()) {
+                    intakeLauncher.powerOnLauncher(launchPower);
+                    follower.followPath(scorePickup2);
+                    setPathState(3);
+                }
+            }
+            case 3 -> {
+                // score line 2 & go to drink gate start round 1
+                if (follower.atPose(scorePose, 1, 1)) {
+                    intakeLauncher.stopIntake();
+                    // score preload
+                    if (!intakeLauncher.isShooting()) {
+                        intakeLauncher.startShooting();
+                    }
+                    intakeLauncher.takeShot(launchPower);
+
+                    // stop shooting and go for drink pick up 1
+                    if (intakeLauncher.getShootingDuration() > shootWaitTime) {
+                        intakeLauncher.stopShooting();
+                        intakeLauncher.runIntake(1, transferPower);
+                        follower.followPath(drinkPickupStart);
+                        setPathState(4);
+                    }
+                }
+            }
+            case 4 -> {
+                // drink balls and go to score pose
+                if (!follower.isBusy()) {
+                    // drink
+                    try {
+                        sleep(drinkWaitTime);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    // go to score pose
+                    intakeLauncher.powerOnLauncher(launchPower);
+                    follower.followPath(drinkPickupScore);
+                    setPathState(5);
+                }
+            }
+            case 5 -> {
+                // score drink 1  & go to drink gate start round 2
+                if (follower.atPose(scorePose, 1, 1)) {
+                    intakeLauncher.stopIntake();
+                    if (!intakeLauncher.isShooting()) {
+                        intakeLauncher.startShooting();
+                    }
+                    intakeLauncher.takeShot(launchPower);
+
+                    if (intakeLauncher.getShootingDuration() > shootWaitTime) {
+                        intakeLauncher.stopShooting();
+                        intakeLauncher.runIntake(1, transferPower);
+                        follower.followPath(drinkPickupStart);
+                        setPathState(6);
+                    }
+                }
+            }
+            case 6 -> {
+                // drink round 2 balls and go to score pose
+                if (!follower.isBusy()) {
+                    // drink
+                    try {
+                        sleep(drinkWaitTime);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    // go to score pose
+                    intakeLauncher.powerOnLauncher(launchPower);
+                    follower.followPath(drinkPickupScore);
+                    setPathState(7);
+                }
+            }
+            case 7 -> {
+                // score the drink 2 and go to line 1 for pick up
+                if (follower.atPose(scorePose, 1, 1)) {
+                    intakeLauncher.stopIntake();
+                    if (!intakeLauncher.isShooting()) {
+                        intakeLauncher.startShooting();
+                    }
+                    intakeLauncher.takeShot(launchPower);
+//                    intakeLauncher.updateShootingLogic(launchPower, follower.getPose());
+
+                    if (intakeLauncher.getShootingDuration() > shootWaitTime) {
+                        intakeLauncher.stopShooting();
+                        intakeLauncher.runIntake(1, transferPower);
+                        intakeLauncher.powerOnLauncher(launchPower);
+                        follower.followPath(grabPickup1, true);
+                        setPathState(8);
+                    }
+                }
+            }
+            case 8 -> {
+                // go to score pose for line 1
+                if (!follower.isBusy()) {
+                    intakeLauncher.runIntake(1, transferPower);
+                    intakeLauncher.powerOnLauncher(launchPower);
+                    follower.followPath(scorePickup1);
+                    setPathState(9);
+                }
+            }
+            case 9 -> {
+                // score line 1 & go to line 3
+                if (follower.atPose(scorePose, 1, 1)) {
+                    intakeLauncher.stopIntake();
+                    // score line 1
+                    if (!intakeLauncher.isShooting()) {
+                        intakeLauncher.startShooting();
+                    }
+                    intakeLauncher.takeShot(launchPower + 0.03);
+//                    intakeLauncher.updateShootingLogic(launchPower, follower.getPose());
+
+                    // stop shooting and go for drink pick up 3
+                    if (intakeLauncher.getShootingDuration() > shootWaitTime) {
+                        intakeLauncher.stopShooting();
+                        intakeLauncher.runIntake(1, transferPower);
+                        follower.followPath(grabPickup3);
+                        setPathState(10);
+                    }
+                }
+            }
+            case 10 -> {
+                // go to score pose for line 3
+                if (!follower.isBusy()) {
+                    intakeLauncher.runIntake(1, transferPower);
+                    follower.followPath(scorePickup3);
+                    intakeLauncher.powerOnLauncher(launchPower);
+                    setPathState(11);
+                }
+            }
+            case 11 -> {
+                // score the line 3 and go to line 1 for park
+                if (follower.atPose(scorePose, 1, 1)) {
+                    intakeLauncher.stopIntake();
+                    if (!intakeLauncher.isShooting()) {
+                        intakeLauncher.startShooting();
+                    }
+                    intakeLauncher.takeShot(launchPower);
+//                    intakeLauncher.updateShootingLogic(launchPower, follower.getPose());
+
+                    if (intakeLauncher.getShootingDuration() > shootWaitTime) {
+                        intakeLauncher.stopShooting();
+//                        intakeLauncher.runIntake(1, transferPower);
+                        follower.followPath(gatePark, true);
+                        setPathState(12);
+                    }
+                }
+            }
+            case 12 -> {
+                if (!follower.isBusy()) {
+                    intakeLauncher.stopIntake();
+                    intakeLauncher.stopShooting();
+                    setPathState(-1);
+                }
+            }
+        }
+    }
+
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    @Override
+    public void loop() {
+        DrawingUtil.drawRobotOnField(field, follower.getPose().getX(), follower.getPose().getY(),
+                follower.getPose().getHeading(), Math.toRadians(intakeLauncher.getCurrentTurnAngle()), GOAL_X, GOAL_Y);
+        follower.update();
+        autonomousPathUpdate();
+//        intakeLauncher.updateShootingLogic(launchPower, follower.getPose());
+
+        intakeLauncher.setTargetTurnAngle(Math.toDegrees(follower.getHeading()));
+        intakeLauncher.updateTurret(follower.getPose());
+
+        Pose currentPose = follower.getPose();
+        blackboard.put("POSE_X", currentPose.getX());
+        blackboard.put("POSE_Y", currentPose.getY());
+        blackboard.put("POSE_HEADING", currentPose.getHeading());
+
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+
+    @Override
+    public void init() {
+        field = PanelsField.INSTANCE.getField();
+        field.setOffsets(PanelsField.INSTANCE.getPresets().getPEDRO_PATHING());
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+
+        follower = Constants.createFollower(hardwareMap);
+        intakeLauncher = new IntakeLauncher(hardwareMap, telemetry, follower);
+        intakeLauncher.setInitialHeading(startPose.getHeading());
+        IntakeLauncher.minTransferThreashhold = 0.95;
+        intakeLauncher.setGoal(GOAL_X, GOAL_Y);
+        buildPaths();
+        follower.setStartingPose(startPose);
+        intakeLauncher.setShooterPIDFCoefficients();
+    }
+
+    @Override
+    public void start() {
+//        intakeLauncher.powerOnLauncher(launchPower);
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+}
