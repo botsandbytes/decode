8a1e0178e8cbbabd750ab3f9390d17fab4b531f3
pokeshah <89949620+pokeshah@users.noreply.github.com>
2026-07-12 15:50:06 -0700

    fix compile time error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
@@ -26,6 +26,7 @@ import static com.pedropathing.math.MathFunctions.quadraticFit;
 import android.annotation.SuppressLint;
 
 import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
