13f028ff4e023f309cd6c948df07ef755d77540a
pokeshah <89949620+pokeshah@users.noreply.github.com>
2026-07-22 20:03:24 -0700

    rename light to lumostest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LumosTest.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.robot.Lumos;
+
+@TeleOp(name = "Lumos Test", group = "Test")
+public class LumosTest extends LinearOpMode {
+  @Override
+  public void runOpMode() throws InterruptedException {
+    Lumos lumos = new Lumos(hardwareMap);
+    waitForStart();
+
+    if (isStopRequested()) return;
+
+    while (opModeIsActive()) {
+      lumos.setColor(Lumos.Color.RED);
+      telemetry.addData("pos", lumos.getPosition());
+    }
+  }
+}
