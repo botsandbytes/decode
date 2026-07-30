09cf1806b536a6ee3c993935284dc4f3957d2e2f
pokeshah <89949620+pokeshah@users.noreply.github.com>
2026-07-30 01:37:24 -0700

    fix unified autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/UnifiedAutos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/UnifiedAutos.java
@@ -1,37 +1,32 @@
 package org.firstinspires.ftc.teamcode.auto;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
-import com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.teamcode.records.Alliance;
 
 public final class UnifiedAutos {
 
-  @OpModeRegistrar
-  public static void register(OpModeManager manager) {
-    manager.register("Blue Auto NEW", BlueAutoNew.class);
-    manager.register("Red Auto NEW", RedAutoNew.class);
-    manager.register("Blue Opposite NEW", BlueOppositeNew.class);
-    manager.register("Red Opposite NEW", RedOppositeNew.class);
-  }
-
+  @Autonomous(name = "Blue Auto NEW", group = "Autonomous")
   public static class BlueAutoNew extends AllianceAutoNew {
     public BlueAutoNew() {
       super(Alliance.BLUE);
     }
   }
 
+  @Autonomous(name = "Red Auto NEW", group = "Autonomous")
   public static class RedAutoNew extends AllianceAutoNew {
     public RedAutoNew() {
       super(Alliance.RED);
     }
   }
 
+  @Autonomous(name = "Blue Opposite NEW", group = "Autonomous")
   public static class BlueOppositeNew extends AllianceOppositeNew {
     public BlueOppositeNew() {
       super(Alliance.BLUE);
     }
   }
 
+  @Autonomous(name = "Red Opposite NEW", group = "Autonomous")
   public static class RedOppositeNew extends AllianceOppositeNew {
     public RedOppositeNew() {
       super(Alliance.RED);
