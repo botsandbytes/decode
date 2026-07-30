c3207b31a745751556296c9b79bc63688bb63ae6
pokeshah <89949620+pokeshah@users.noreply.github.com>
2026-07-30 01:43:27 -0700

    supress locale error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TurnAnalogTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TurnAnalogTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.tests;
 
+import android.annotation.SuppressLint;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
@@ -42,6 +44,7 @@ public class TurnAnalogTest extends OpMode {
     telemetry.update();
   }
 
+  @SuppressLint("DefaultLocale")
   @Override
   public void loop() {
     if (turnAnalog == null) {
